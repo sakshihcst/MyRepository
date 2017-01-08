47c21fa5a27e0f32861ad5573205d2a132d90ac4
sakshihcst <sakshihcst@gmail.com>
2017-01-08 13:58:54 -0800

    Delete

--- MPUWEBServices/src/main/java/com/searshc/mpuwebservice/processor/AdditionalServiceProcessor.java
+++
@@ -1,7 +0,0 @@
-package com.searshc.mpuwebservice.processor;
-
-import com.sears.dj.common.exception.DJException;
-
-public interface AdditionalServiceProcessor {
-	public void sendFinalResponseManual(String[] salescheckList,String dbNum) throws DJException;
-}
--- MPUWEBServices/src/main/java/com/searshc/mpuwebservice/processor/impl/MpuWebDlvryStatusService.java
+++
@@ -1,9 +0,0 @@
-package com.searshc.mpuwebservice.processor.impl;
-
-import com.sears.dj.common.exception.DJException;
-
-public interface MpuWebDlvryStatusService {
-
-	
-	public void sendMpuInStorePurchaseNotification(String sywrId,String store,String salescheck) throws DJException;
-}
